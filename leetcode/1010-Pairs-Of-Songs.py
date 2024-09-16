494c36e3cbc854254466984b3965ad5114e9b57e
Andrew Smithwick <andy.smithwick@gmail.com>
2024-09-15 23:16:59 -0700

    Update 1010-Pairs-Of-Songs.py

--- leetcode/1010-Pairs-Of-Songs.py
+++ leetcode/1010-Pairs-Of-Songs.py
@@ -4,12 +4,15 @@ class Solution:
         numPairs = 0
 
         for dur in time:
+            # We will only get pairs (time[j],time[i]) where index j > index i
+            # Because we only check for target values that come before current value
+            # target value = (60 - dur) % 60
             numPairs += numLength[(60 - dur) % 60]
 
             # We add the curr value to the array after we change number of pairs, b/c we don't want to create a pair with ourselves
             numLength[dur % 60] += 1
 
-            # print(f'{numPairs} | {dur} {dur%60} | {(60 - (dur%60))}')
+            print(f'{numPairs} | {dur} | {(60 - dur) % 60}')
             # print(numLength)
 
         return numPairs
